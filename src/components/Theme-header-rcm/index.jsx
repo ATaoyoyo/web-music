d1b846acd58c57cc8f38672ffa9ac345955db3c8
ATaoyoyo <1029187998@qq.com>
2021-04-10 18:03:57 +0800

    fix: 样式修复

--- src/components/Theme-header-rcm/index.jsx
+++ src/components/Theme-header-rcm/index.jsx
@@ -6,7 +6,7 @@ import { HeaderWrapper } from "./styled";
 const ThemeHeaderRcm = (props) => {
   const { title, keywords } = props;
   return (
-    <HeaderWrapper>
+    <HeaderWrapper className="sprite_02">
       <div className="left">
         <h3 className="title">{title}</h3>
         <div className="keyword">
